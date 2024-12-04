cb0113d48bb21dada811d45435ebcb9c95439435
gabrielsob <bielgds07@gmail.com>
2024-12-04 10:35:13 -0300

    Desafio

---
+++ index.js
@@ -0,0 +1,29 @@
+let xp = 10001
+let heroi = "felipao"
+
+if (xp <= 1000) {
+    xp = "ferro"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+}else if (xp <= 2000) {
+    xp = "Bronze"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+
+}else if (xp <= 5000){
+    xp = "Prata"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+}else if (xp <= 7000){
+    xp = "Ouro"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+}else if (xp <= 8000){
+    xp = "Platina"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+}else if (xp <= 9000){
+    xp = "Ascendente"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+}else if (xp <= 10000){
+    xp = "Imortal"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+}else if (xp >= 10001){
+    xp = "Radiante"
+    console.log("O Herói de nome " +  heroi + " está no nível de " + xp);
+}
